311b2ee9ffcd8c13b5034c63fa502714542191aa
Choton Chandra Bhowmik <83583216+chotonbhowmik@users.noreply.github.com>
2025-04-01 23:05:11 +0600

    one form field hide

--- src/pages/Home/BannerSection/BannerSection.jsx
+++ src/pages/Home/BannerSection/BannerSection.jsx
@@ -1,5 +1,5 @@
 import BannerBg from "../../../assets/banner.jpg";
-import star from "../../../assets/start.png";
+// import star from "../../../assets/start.png";
 import number from "../../../assets/24.png";
 const BannerSection = () => {
   return (
@@ -15,19 +15,19 @@ const BannerSection = () => {
       <div className="hero-overlay "></div>
       <div className="hero-content text-neutral-content text-center flex justify-center items-center pb-20">
         <div className="max-w-7xl">
-          <div className="justify-self-end">
+          {/* <div className="justify-self-end">
             <img src={star} alt="Star" className="w-32 h-auto" />
-          </div>
+          </div> */}
           <img src={number} className="object-cover"></img>
           <h1 className="mb-5 text-[30px] lg:text-[72px]  font-bold text-[#FFFFFF]  leading-[40px] lg:leading-[80px]">
             Sorgenfrei Lagerraum mieten in Wien-Landstrasse
           </h1>
 
-          <div className="bg-white lg:rounded-full rounded-bl-[30px] rounded-br-[30px] rounded-tl-[30px] rounded-tr-[30px] max-w-4xl mx-auto p-6">
+          <div className="bg-white lg:rounded-full rounded-bl-[30px] rounded-br-[30px] rounded-tl-[30px] rounded-tr-[30px] max-w-2xl mx-auto p-6">
             <form className="w-full">
               <div className="flex flex-wrap justify-between space-y-2 md:space-y-0 md:flex-row gap-5">
                 {/* LOCATION FIELD */}
-                <div className="relative z-0 w-full md:w-64 group border-b-2 lg:border-r-2 lg:border-b-0 border-slate-300 pb-5 lg:pb-0">
+                {/* <div className="relative z-0 w-full md:w-64 group border-b-2 lg:border-r-2 lg:border-b-0 border-slate-300 pb-5 lg:pb-0">
                   <select
                     id="location"
                     name="location"
@@ -47,7 +47,7 @@ const BannerSection = () => {
                     STANDORT WÄHLEN
                   </label>
                   <i className="fa fa-long-arrow-down absolute right-3 top-1/2 -translate-y-1/2 text-[#1942BB] pointer-events-none"></i>
-                </div>
+                </div> */}
 
                 <div className="relative z-0 w-full md:w-64 group lg:border-r-2 lg:border-b-0 border-slate-300 ">
                   <select
@@ -85,7 +85,7 @@ const BannerSection = () => {
               </div>
             </form>
           </div>
-          <div className="bg-[#00C6AA] lg:max-w-[650px] max-w-[310px] rounded-bl-[30px] rounded-br-[30px] text-white mx-auto text-center">
+          <div className="bg-[#00C6AA] lg:max-w-[450px] max-w-[310px] rounded-bl-[30px] rounded-br-[30px] text-white mx-auto text-center">
             <p className="text-[16px] font-normal py-2">
               Nur 10 Box verfügbar. Ab 59:00 EUR pro Monat. Buchen Sie Ihre Box
               jetzt
